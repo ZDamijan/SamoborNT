6d028a9edb80f1c8349c0e1f668591b1077550e6
ZDamijan <zlatko.damijanic@gmail.com>
2021-04-16 16:59:01 +0200

    icon and name changed

--- ARImageTracer/androidBuild/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
--- ARImageTracer/androidBuild/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "il2cpp-config.h"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\BlobReader.cpp"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\Debugger.cpp"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\VmStringUtils.cpp"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System.Threading\Interlocked.cpp"
-#include "F:\Documents\AndroidProjects\SamoborNT\ARImageTracer\androidBuild\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System\Math.cpp"
