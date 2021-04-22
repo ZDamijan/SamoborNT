56d3c9f368b8a4e9c4b5333a5a9d082b3842a8a2
ZDamijan <zlatko.damijanic@gmail.com>
2021-04-22 16:25:06 +0200

    IL2CPP build

---
+++ samobornt-ar/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/UnityEngine.ImageConversionModule_CodeGen.c
@@ -0,0 +1,47 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Byte[] UnityEngine.ImageConversion::EncodeToPNG(UnityEngine.Texture2D)
+extern void ImageConversion_EncodeToPNG_mA598C2969C878ACC5AEA5FDC92C6199EB30D51ED (void);
+static Il2CppMethodPointer s_methodPointers[1] = 
+{
+	ImageConversion_EncodeToPNG_mA598C2969C878ACC5AEA5FDC92C6199EB30D51ED,
+};
+static const int32_t s_InvokerIndices[1] = 
+{
+	3492,
+};
+extern const CustomAttributesCacheGenerator g_UnityEngine_ImageConversionModule_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_ImageConversionModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_ImageConversionModule_CodeGenModule = 
+{
+	"UnityEngine.ImageConversionModule.dll",
+	1,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_UnityEngine_ImageConversionModule_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
