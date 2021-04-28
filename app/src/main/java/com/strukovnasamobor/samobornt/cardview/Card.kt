15e542e09ea7e9b201e1ac44449282e7ac20fd82
Drazo420 <80947246+Drazo420@users.noreply.github.com>
2021-04-28 21:13:48 +0200

    Added Card data class

---
+++ app/src/main/java/com/strukovnasamobor/samobornt/cardview/Card.kt
@@ -0,0 +1,15 @@
+package com.strukovnasamobor.samobornt.cardview
+
+data class Card (
+
+    var Card_id: Int,
+
+    var Main_image: Int,
+
+    var Location_name: String,
+
+    var Short_description: String,
+
+    var Long_description: String,
+
+    )
