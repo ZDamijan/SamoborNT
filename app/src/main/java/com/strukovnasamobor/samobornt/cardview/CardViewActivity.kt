23ac2092a07c0188670934a67ddf40f70df243c7
Drazo420 <80947246+Drazo420@users.noreply.github.com>
2021-04-29 20:03:03 +0200

    Updated hashMap

--- app/src/main/java/com/strukovnasamobor/samobornt/cardview/CardViewActivity.kt
+++ app/src/main/java/com/strukovnasamobor/samobornt/cardview/CardViewActivity.kt
@@ -35,7 +35,7 @@ class CardViewActivity : BaseActivity() {
                 "Samoborski muzej" to R.drawable.muzej_t_1,
                 "Crkva sv. Anastazije" to R.drawable.anastazija_t_1,
                 "Glazbena škola Ferdo Livadić" to R.drawable.livadic_t_1,
-                "Parking kod kina (polazišna točka)" to R.drawable.parking_t_1,
+                "Parking kod kina" to R.drawable.parking_t_1,
                 "Drveni most Mala Venecija" to R.drawable.mvenecija_t_1,
                 "Franjevačka crkva i Tabor" to R.drawable.franjevci_t_1,
                 "Vungriščak" to R.drawable.vungri_t_1,
@@ -43,7 +43,7 @@ class CardViewActivity : BaseActivity() {
                 "Park domovinske zahvalnosti" to R.drawable.pdz_t_1,
                 "Trg kralja Tomislava" to R.drawable.trg_t_1,
                 "Stari grad" to R.drawable.starigrad_t_1,
-                "Tancplac Anindol - Kapela sv. Ane" to R.drawable.svana_t_1,
+                "Kapela sv. Ane" to R.drawable.svana_t_1,
                 "Vidikovac" to R.drawable.vidikovac_t_1,
         )
         val cursor: Cursor = connection!!.getFetchAllCursor()
