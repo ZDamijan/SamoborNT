d1549dc129c4e7c773b5e453fb36168945c69683
JuricaRT <runtas.j@gmail.com>
2021-05-02 09:46:57 +0200

    restore RecyclerView state

--- app/src/main/java/com/strukovnasamobor/samobornt/cardview/CardViewActivity.kt
+++ app/src/main/java/com/strukovnasamobor/samobornt/cardview/CardViewActivity.kt
@@ -1,8 +1,8 @@
 package com.strukovnasamobor.samobornt.cardview
 
-import android.content.Intent
 import android.database.Cursor
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.annotation.DrawableRes
 import androidx.recyclerview.widget.RecyclerView
 import com.strukovnasamobor.samobornt.BaseActivity
@@ -10,24 +10,37 @@ import com.strukovnasamobor.samobornt.R
 import com.strukovnasamobor.samobornt.detail.DetailActivity
 import com.strukovnasamobor.samobornt.services.*
 
+private var recyclerViewState: Parcelable? = null
+
 class CardViewActivity : BaseActivity() {
     private var connection: DBConnection? = null
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.card_view)
         super.initializeMenu()
+        setContentView(R.layout.card_view)
+        recyclerView = findViewById(R.id.recycler_view)
         connection = DBConnection.getConnectionInstance(this)
         val cardsList: MutableList<Card> = createCards()
         val cardsViewAdapter = CardViewAdapter({ card -> adapterOnClick(card) }, cardsList)
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
         recyclerView.adapter = cardsViewAdapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+        recyclerView.layoutManager!!.onRestoreInstanceState(recyclerViewState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        recyclerViewState = recyclerView.layoutManager!!.onSaveInstanceState()
     }
 
     private fun createCards(): MutableList<Card> {
         val cardsList: MutableList<Card> = mutableListOf()
         val cursor: Cursor = connection!!.getFetchAllCursor()
-        var id: Int = 1
+        var id = 1
         if (cursor.moveToFirst()) {
             while (!cursor.isAfterLast) {
                 val locationName: String = cursor.getString(cursor.getColumnIndex(C_NAME))
@@ -37,9 +50,9 @@ class CardViewActivity : BaseActivity() {
                 @DrawableRes
                 val locationImageID: Int? = this.resources
                     .getIdentifier(imageName, "drawable", this.packageName)
-                val newCard = Card(cardID=id, locationName=locationName,
-                        longDescription=locationLD, shortDescription=locationSD,
-                        mainImage=locationImageID)
+                val newCard = Card(cardID = id, locationName = locationName,
+                        longDescription = locationLD, shortDescription = locationSD,
+                        mainImage = locationImageID)
                 cardsList.add(newCard)
                 cursor.moveToNext()
                 id += 1
