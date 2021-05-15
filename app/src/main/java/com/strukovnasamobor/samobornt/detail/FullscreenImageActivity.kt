31301e25b9f87db69cae1d4bb9d8359d62634190
JuricaRT <runtas.j@gmail.com>
2021-05-15 13:36:52 +0200

    make images fullscreen on click

---
+++ app/src/main/java/com/strukovnasamobor/samobornt/detail/FullscreenImageActivity.kt
@@ -0,0 +1,30 @@
+package com.strukovnasamobor.samobornt.detail
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.ImageView
+import com.strukovnasamobor.samobornt.BaseActivity
+import com.strukovnasamobor.samobornt.R
+
+class FullscreenImageActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        super.initializeMenu()
+        supportActionBar!!.hide()
+
+        setContentView(R.layout.detail_image)
+
+        val imageView: ImageView = findViewById(R.id.image)
+        imageView.setImageResource(intent!!.extras!!.getInt("imageResource"))
+
+        @Suppress("DEPRECATION")
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        imageView.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+        imageView.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+    }
+}
--- app/src/main/java/com/strukovnasamobor/samobornt/detail/SwipeAdapter.kt
+++ app/src/main/java/com/strukovnasamobor/samobornt/detail/SwipeAdapter.kt
@@ -1,6 +1,7 @@
 package com.strukovnasamobor.samobornt.detail
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,31 +10,35 @@ import android.widget.LinearLayout
 import androidx.viewpager.widget.PagerAdapter
 import com.strukovnasamobor.samobornt.R
 
-class SwipeAdapter(var context: Context, var images:IntArray):PagerAdapter(){
-    lateinit var layoutInflater: LayoutInflater
 
+class SwipeAdapter(var context: Context, private var images: IntArray) : PagerAdapter() {
+    private var layoutInflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
        return view ===`object` as LinearLayout
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val view=layoutInflater.inflate(R.layout.detail_image,container,false)
+        val view = layoutInflater.inflate(R.layout.detail_image, container,false)
         val imageView = view.findViewById<View>(R.id.image) as ImageView
         imageView.setImageResource(images[position])
 
+        imageView.setOnClickListener {
+            val intent = Intent(context, FullscreenImageActivity::class.java)
+            intent.putExtra("imageResource", images[position])
+            context.startActivity(intent)
+        }
+
         container.addView(view)
 
-            return view
+        return view
     }
-    init {
-        layoutInflater=context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-    }
+
     override fun getCount(): Int {
         return images.size
     }
+
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
         container.removeView(`object` as LinearLayout)
     }
-
 }
